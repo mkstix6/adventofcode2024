61c55997ea3f47d65cd78e8ab1d1788d7af4141c
Mark Stickling <mark.stickling@itv.com>
2024-12-01 17:58:17 +0000

    Day01 Part1 solution

--- day01/main.ts
+++ day01/main.ts
@@ -1,8 +1,28 @@
-export function add(a: number, b: number): number {
-  return a + b;
+// deno get input contents from file and parse it
+const input = Deno.readTextFileSync("input.txt");
+
+export function day01part1(input: string): number {
+  // split the columns of input into separate arrays
+  const column1: number[] = [];
+  const column2: number[] = [];
+  for (const line of input.split("\n")) {
+    const [a, b] = line.split("   ").map(Number);
+    column1.push(a);
+    column2.push(b);
+  }
+
+  column1.sort();
+  column2.sort();
+
+  let result: number = 0;
+
+  for (let i = 0; i < column1.length; i++) {
+    result += Math.abs(column1[i] - column2[i]);
+  }
+  return result;
 }
 
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+  console.log("Answer for day 1", day01part1(input));
 }
--- day01/main_test.ts
+++ day01/main_test.ts
@@ -1,6 +1,15 @@
 import { assertEquals } from "@std/assert";
-import { add } from "./main.ts";
+import { day01part1 } from "./main.ts";
 
-Deno.test(function addTest() {
-  assertEquals(add(2, 3), 5);
+const testInput = `3   4
+4   3
+2   5
+1   3
+3   9
+3   3`;
+
+const testExpected = 11;
+
+Deno.test(function day01part1Test() {
+  assertEquals(day01part1(testInput), testExpected);
 });
