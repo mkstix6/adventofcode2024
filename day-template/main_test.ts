fd9d4b7c5155edc689fdea58beac472d8469aafb
Mark Stickling <mark.stickling@itv.com>
2024-12-02 12:29:43 +0000

    Create template for each day's puzzles

---
+++ day-template/main_test.ts
@@ -0,0 +1,14 @@
+import { assertEquals } from "@std/assert";
+import { dayXXpart1, dayXXpart2 } from "./main.ts";
+
+const testInput = ``;
+
+Deno.test(function dayXXpart1Test() {
+  const testPart1Expected = 0;
+  assertEquals(dayXXpart1(testInput), testPart1Expected);
+});
+
+Deno.test(function dayXXpart2Test() {
+  const testPart2Expected = 0;
+  assertEquals(dayXXpart2(testInput), testPart2Expected);
+});
