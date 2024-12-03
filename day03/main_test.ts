7bb9227aeef329d42cf79a2fb732040e94949090
Mark Stickling <mark.stickling@itv.com>
2024-12-03 11:44:54 +0000

    Day03 ready to start

---
+++ day03/main_test.ts
@@ -0,0 +1,14 @@
+import { assertEquals } from "@std/assert";
+import { day03part1, day03part2 } from "./main.ts";
+
+const testInput = ``;
+
+Deno.test(function day03part1Test() {
+  const testPart1Expected = 0;
+  assertEquals(day03part1(testInput), testPart1Expected);
+});
+
+Deno.test(function day03part2Test() {
+  const testPart2Expected = 0;
+  assertEquals(day03part2(testInput), testPart2Expected);
+});
