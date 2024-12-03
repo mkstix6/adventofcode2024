a4781dad001e458997ed28fdb1c6122fbc049958
Mark Stickling <mark.stickling@itv.com>
2024-12-03 12:39:16 +0000

    Improve daily template

--- day-template/main.ts
+++ day-template/main.ts
@@ -1,4 +1,3 @@
-// deno get input contents from file and parse it
 const input = Deno.readTextFileSync("input.txt");
 
 export function dayXXpart1(input: string): number {
@@ -9,8 +8,7 @@ export function dayXXpart2(input: string): number {
   return 0;
 }
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  console.log("Answer for day 02 part 1", dayXXpart1(input));
-  console.log("Answer for day 02 part 2", dayXXpart2(input));
+  console.log("Answer for dayXX part1", dayXXpart1(input));
+  console.log("Answer for dayXX part2", dayXXpart2(input));
 }
