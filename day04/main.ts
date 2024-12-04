c5f6fce85ca6ad2809ef10f5462149a62083a816
Mark Stickling <mark.stickling@itv.com>
2024-12-04 21:44:32 +0000

    Day04 Part1 solution

--- day04/main.ts
+++ day04/main.ts
@@ -1,7 +1,48 @@
 const input = Deno.readTextFileSync("input.txt");
 
+type Coordinates = [number, number];
+/**
+ * Find `XMAS` in the word search.
+ */
 export function day04part1(input: string): number {
-  return 0;
+  const target: string = "XMAS";
+  const targetChars = target.split("");
+  const matrix = input.split("\n").map((line) => [...line]);
+  const stepCoordinates: Coordinates[] = [
+    [-1, -1], // top left
+    [-1, 0], // top
+    [-1, 1], // top right
+    [0, -1], // left
+    [0, 1], // right
+    [1, -1], // bottom left
+    [1, 0], // bottom
+    [1, 1], // bottom right
+  ];
+
+  let count: number = 0;
+
+  for (let i = 0; i < matrix.length; i++) {
+    const row = matrix[i];
+    for (let j = 0; j < row.length; j++) {
+      const startChar = matrix[i][j];
+      if (startChar !== targetChars[0]) {
+        continue;
+      }
+      // check in each of the 8 surrounding directions
+      stepCoordinates.forEach(([x, y]: Coordinates, index: number): void => {
+        for (let k = 0; k < targetChars.length; k++) {
+          const nextChar = matrix[i + (k + 1) * x]?.[j + (k + 1) * y];
+          if (!nextChar || nextChar !== targetChars[k + 1]) {
+            break;
+          }
+          if (k === targetChars.length - 2) {
+            count++;
+          }
+        }
+      });
+    }
+  }
+  return count;
 }
 
 export function day04part2(input: string): number {
--- day04/main_test.ts
+++ day04/main_test.ts
@@ -1,14 +1,32 @@
 import { assertEquals } from "@std/assert";
 import { day04part1, day04part2 } from "./main.ts";
 
-const testInput = ``;
-
 Deno.test(function day04part1Test() {
-  const testPart1Expected = 0;
+  const testInput = `MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX`;
+  const testPart1Expected = 18;
   assertEquals(day04part1(testInput), testPart1Expected);
 });
 
 Deno.test(function day04part2Test() {
-  const testPart2Expected = 0;
+  const testInput = `.M.S......
+                    ..A..MSMS.
+                    .M.S.MAA..
+                    ..A.ASMSM.
+                    .M.S.M....
+                    ..........
+                    S.S.S.S.S.
+                    .A.A.A.A..
+                    M.M.M.M.M.
+                    ..........`;
+  const testPart2Expected = 9;
   assertEquals(day04part2(testInput), testPart2Expected);
 });
