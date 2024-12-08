e2bd7056e10d7668c3f50a36ed8f0d3a9e9f92df
Mark Stickling <mark.stickling@itv.com>
2024-12-08 14:21:53 +0000

    Day07 Part1 teast set up

--- day07/main_test.ts
+++ day07/main_test.ts
@@ -1,10 +1,18 @@
 import { assertEquals } from "@std/assert";
 import { day07part1, day07part2 } from "./main.ts";
 
-const testInput = ``;
+const testInput = `190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20`;
 
 Deno.test(function day07part1Test() {
-  const testPart1Expected = 0;
+  const testPart1Expected = 3749;
   assertEquals(day07part1(testInput), testPart1Expected);
 });
 
