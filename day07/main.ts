388f808e58228c60c8ae120046ee6403b0ef834c
Mark Stickling <mark.stickling@itv.com>
2024-12-08 14:04:58 +0000

    Day07 ready to start

---
+++ day07/main.ts
@@ -0,0 +1,14 @@
+const input = Deno.readTextFileSync("input.txt");
+
+export function day07part1(input: string): number {
+  return 0;
+}
+
+export function day07part2(input: string): number {
+  return 0;
+}
+
+if (import.meta.main) {
+  console.log("Answer for day07 part1", day07part1(input));
+  console.log("Answer for day07 part2", day07part2(input));
+}
---
+++ day07/main_test.ts
@@ -0,0 +1,14 @@
+import { assertEquals } from "@std/assert";
+import { day07part1, day07part2 } from "./main.ts";
+
+const testInput = ``;
+
+Deno.test(function day07part1Test() {
+  const testPart1Expected = 0;
+  assertEquals(day07part1(testInput), testPart1Expected);
+});
+
+Deno.test(function day07part2Test() {
+  const testPart2Expected = 0;
+  assertEquals(day07part2(testInput), testPart2Expected);
+});
